47318b2ded733b99efc7b43cd20fa16d8d408abf
Edikan Nyah <edikannyah1271@gmail.com>
2025-07-15 15:25:44 -0700

    Moved context files into src/components and fixed imports

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import UserContext from './components/UserContext';
+import ProfilePage from './components/ProfilePage';
 
 function App() {
   const userData = {
