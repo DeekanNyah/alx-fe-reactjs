c95a571e480f3c160346b828d62318e8fc26ac0c
Edikan Nyah <edikannyah1271@gmail.com>
2025-07-15 12:38:15 -0700

    Add inline CSS to Header, UserProfile, MainContent, and Footer components

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>This is a paragraph below the heading.</p>
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
