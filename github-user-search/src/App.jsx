a95bfba57ed4b4136ad1b34046e2cdb48225efde
Edikan Nyah <edikannyah1271@gmail.com>
2025-08-07 08:29:08 -0700

    Add GitHub User Search React App

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <p>Start building your search interface here.</p>
+    </div>
+  );
+}
+
+export default App;
