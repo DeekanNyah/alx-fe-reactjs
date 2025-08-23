ce4d40ed7c6321e0d048c6e683fb222cb5656ca9
Edikan Nyah <edikannyah1271@gmail.com>
2025-08-22 17:37:53 -0700

    Add dynamic route /blog/:id and implement ProtectedRoute component

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import BlogPost from "./components/BlogPost";
@@ -7,30 +7,24 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
 
-        {/* Protected profile route with nested routes */}
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
+      {/* Dynamic route */}
+      <Route path="/blog/:id" element={<BlogPost />} />
 
-        {/* Dynamic blog route */}
-        <Route path="/blog/:postId" element={<BlogPost />} />
+      {/* Protected route for Profile */}
+      <Route
+        path="/profile/*"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
 
-        {/* Login */}
-        <Route path="/login" element={<Login />} />
-
-        {/* Fallback redirect */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
